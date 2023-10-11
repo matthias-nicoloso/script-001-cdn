4be6d56904a336f9924b2cb2c34cf35eb99e543d
Matthias Nicoloso <matthias.nicoloso@heig-vd.ch>
2023-10-11 21:15:59 +0200

    Filled .gitignore and header

---
+++ cdn.py
@@ -0,0 +1,13 @@
+# 
+# cdn - cdnim, 2022-2023 https://github.com/matthias-nicoloso/script-001-cdn
+# 
+# A bash/python-based program that allows an in-depth 'cd' command management
+# through customisable locations. Each of the locations added are written in a 
+# data file and can be easily managed by the program.
+#
+# (c) Matthias Nicoloso <matthias.nicoloso@heig-vd.ch> <matthiasnicoloso@gmail.com>
+# 
+# "I just wanted to learn electronics, leave me alone" 
+#                   - Matthias Nicoloso, 3rd semester at the HEIG-VD
+# 
+
